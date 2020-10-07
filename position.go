9e7af2b643c68d2e84a7de29b2d77d6d52026999
8bitstout <conor.murphy4304@gmail.com>
2020-10-06 20:43:47 -0400

    added position type and respective constants to own file

---
+++ position.go
@@ -0,0 +1,12 @@
+package main
+
+type Position int8
+
+const (
+	POSITION_SMALL_BLIND Position = 0
+	POSITION_BIG_BLIND   Position = 1
+)
+
+func (p Position) String() string {
+	return [...]string{"Small blind", "Big blind"}[p]
+}
