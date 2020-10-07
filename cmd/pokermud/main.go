0473020513e312f72c85d876e4240f1ca91d68c1
8bitstout <conor.murphy4304@gmail.com>
2020-10-07 19:35:49 -0400

    moved main to its own folder and added logic for running server/client

--- cmd/pokermud/main.go
+++ cmd/pokermud/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/8bitstout/pokermud"
+import (
+	"fmt"
+	"github.com/8bitstout/pokermud"
+	client2 "github.com/8bitstout/pokermud/client"
+	server2 "github.com/8bitstout/pokermud/server"
+	"os"
+)
 
 func main() {
 	var players []*pokermud.Player
+	arguments := os.Args
 
 	names := []string{"WCGRider", "OMGClayAiken", "Sauce123", "Ben86"}
 
@@ -11,7 +18,21 @@ func main() {
 		p := pokermud.MakePlayer(name)
 		players = append(players, p)
 	}
-	g := pokermud.MakeGame(players)
-	g.Start()
+	//g := pokermud.MakeGame(players)
+	//g.Start()
+
+	command := arguments[1]
+
+	fmt.Println(arguments[1])
+
+	if command == "server" {
+		s := server2.MakeServer("127.0.0.1:1234")
+		s.Start()
+	}
+
+	if command == "client" {
+		client := client2.MakeClient("127.0.0.1:1234")
+		client.Connect()
+	}
 
 }
