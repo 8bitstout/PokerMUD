bd59d625de7476df2b38f40b7b02222edafb09b3
8bitstout <conor.murphy4304@gmail.com>
2020-10-12 20:54:00 -0400

    added logic to handle action requests

--- tcp/client.go
+++ tcp/client.go
@@ -55,29 +55,66 @@ func (c *Client) Connect() {
 	c.logInfo.Println("Connected to server")
 	c.Connection = connection
 	c.Authenticate()
-	for {
+	for c.Connection != nil {
 		buffer := make([]byte, DEFAULT_BUFFER_SIZE)
 		length, err := c.Connection.Read(buffer)
 		if err != nil {
-			c.logInfo.Println(err)
+			log.Fatal(err)
 		}
-		msg, msgType := ParseMessage(buffer, length)
-		if msgType == 5 {
-			fmt.Println("Dealing cards...")
-			time.Sleep(time.Second * 5)
-			fmt.Println(fmt.Println("Your hand:", string(msg)))
-		}
-		if msgType == 6 {
-			c.logInfo.Println("Connection terminated by server")
+		if length == 0 {
+			c.logInfo.Println("Breaking loop")
 			break
 		}
-		if msgType == 7 {
-			fmt.Println("Dealing the flop...")
-			time.Sleep(time.Second * 3)
-			fmt.Println(fmt.Println(string(msg)))
+		if length > 0 {
+			msg, msgType := ParseMessage(buffer, length)
+			if msgType == 5 {
+				fmt.Println("Dealing cards...")
+				time.Sleep(time.Second * 3)
+				fmt.Println("Your hand:", string(msg))
+			}
+			if msgType == 6 {
+				c.logInfo.Println("Connection terminated by server")
+				c.Connection.Close()
+				c.Connection = nil
+			}
+			if msgType == 7 {
+				fmt.Println("Dealing the flop...")
+				time.Sleep(time.Second * 3)
+				fmt.Println(string(msg))
+			}
+			if msgType == 10 {
+				c.logInfo.Println("Received standard message from server")
+				fmt.Println(string(msg))
+			}
+			if msgType == MESSAGE_ACTION {
+				c.logInfo.Println("Action requested from server")
+				fmt.Println("Enter one of the following commands to send an action\n1. fold\n2. check\n3. bet (amount e.g 10)")
+				reader := bufio.NewReader(os.Stdin)
+				fmt.Print(">> ")
+				response := ""
+				switch cmd, _ := reader.ReadString('\n'); cmd {
+				case "fold\n":
+					response = fmt.Sprint(c.Username, "folded their hand")
+				case "check\n":
+					response = fmt.Sprint(c.Username, "checked their hand")
+				case "bet\n":
+					{
+						fmt.Println("Enter a bet size (e.g. 10)")
+						fmt.Print(">> ")
+						reader.Reset(os.Stdin)
+						i, _ := reader.ReadString('\n')
+						response = fmt.Sprintf("%s bet $%s", c.Username, i)
+					}
+				default:
+					fmt.Println("Command not recognized")
+				}
+				os.Stdin.Close()
+				c.Connection.Write([]byte(response))
+			}
 		}
+
+		buffer = make([]byte, DEFAULT_BUFFER_SIZE)
 	}
-	c.Connection = nil
 }
 
 func MakeClient(port string) *Client {
